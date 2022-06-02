5d061c0418d39bbc3780849fa8584b891bc4420d
Sarkhanas <daniilzon@mail.ru>
2022-06-02 15:22:36 +0300

    to up button added

--- js/index.js
+++ js/index.js
@@ -90,3 +90,26 @@ function messageValid(message) {
     alert("Write a message");
     return false;
 }
+
+//To up button
+$(function() {
+    // при нажатии на кнопку scrollup
+    $('.scrollup').click(function() {
+        // переместиться в верхнюю часть страницы
+        $("html, body").animate({
+            scrollTop:0
+        },1000);
+    })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop()>200) {
+        // то сделать кнопку scrollup видимой
+        $('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+        $('.scrollup').fadeOut();
+    }
+});
